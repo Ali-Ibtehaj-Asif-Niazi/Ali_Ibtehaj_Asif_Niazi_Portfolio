303fe81132431dd580d8ec41caf6525f07e622e4
Ali Ibtehaj Asif Niazi <aliibtehajasifniazi@gmail.com>
2023-08-18 00:36:20 +0500

    update-search

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -9,9 +9,8 @@ export const GET = async (request, { params }) => {
 
         const prompts = await Prompt.find({creator:params.id}).populate('creator');
 
-        return new Response(JSON.stringify(prompts),{
-            status:200})
-        }catch(error){
-            return new Response("Failed to fetch all prompts", { status: 200 })
+        return new Response(JSON.stringify(prompts),{ status:200})
+        } catch (error) {
+            return new Response("Failed to fetch all prompts", { status: 500 })
         }
 }
