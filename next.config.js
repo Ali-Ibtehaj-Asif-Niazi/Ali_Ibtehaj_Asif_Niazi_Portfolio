40a490709753a6313005892984a092e234a04f38
Ali Ibtehaj Asif Niazi <aliibtehajasifniazi@gmail.com>
2023-08-17 23:38:32 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -2,3 +2,16 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: "lh3.googleusercontent.com",
+          port: '',
+          pathname: '/a/AAcHTtcyNp-z16GY3IG42rigzymoSyP7gGgnkw_GB1RKKz590A=s96-c',
+        },
+      ],
+    },
+  }
