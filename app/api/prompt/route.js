512e972b6aa9df72ea43c605cb4ebca34e99f9ae
Ali Ibtehaj Asif Niazi <aliibtehajasifniazi@gmail.com>
2023-10-30 23:43:34 +0500

    wrong HTTP Search

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -14,16 +14,3 @@ export const GET = async (request) => {
         return new Response("Failed to fetch all prompts", { status: 500 });
     }
 };
-export const SEARCH = async (request) => {
-    try {
-      await connectToDatabase();
-      const prompts = await Prompt.find({})
-        .populate("creator")
-        .sort({ createdAt: -1 });
-  
-      return new Response(JSON.stringify(prompts), { status: 200 });
-    } catch (error) {
-      console.log(error);
-      return new Response("Failed to fetch all prompts", { status: 500 });
-    }
-  };
